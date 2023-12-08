5ca0754383bc6176f00824642256907679b75006
1431293 <150165896+1431293@users.noreply.github.com>
2023-12-08 01:33:39 +0000

    Add files via upload

---
+++ exponential_virion_graph.R
@@ -0,0 +1,16 @@
+library("ggplot2")
+dataset<- read.csv("Cui_etal2014.csv", header=TRUE)
+
+attach(dataset)
+
+
+model1 <- lm(data=dataset, log(Virion.volume..nm.nm.nm.)~log(Genome.length..kb.))
+
+model1
+
+summary(model1)
+
+ggplot(data=dataset, aes(x=log(Genome.length..kb.), y=log(Virion.volume..nm.nm.nm.)))+
+  geom_point()+
+  geom_smooth(method=lm) +
+  labs(x="log[Genome length(kb)]", y="log[Virion volume(nm3)]")
